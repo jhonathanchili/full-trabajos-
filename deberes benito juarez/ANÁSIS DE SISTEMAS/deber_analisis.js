56bf9fe98387a5093f553ce7c5093e551a847835
jhonathan <53637124+jhonathanchili@users.noreply.github.com>
2020-06-19 20:22:12 -0500

    Add files via upload

---
+++ deberes benito juarez/ANÁSIS DE SISTEMAS/deber_analisis.js
@@ -0,0 +1,82 @@
+var notas = [ 2, 4, 1, 3, 5,];
+// encotrar la nota mayor e iprimir en laconsola.
+console.log(notas[0]);
+console.log(notas[4]);
+console.log(notas[5]);
+console.log(notas[lenht-1]);
+//combo de elementos.
+notas = [4, 1, 3, 6, 5, 2,];
+         notas[0]= 9;
+notas=[4, 1, 3, 6, 5, 2,];
+
+
+var segundoElemento = notas[1];
+//llamar de elementos navegar a travez.
+notas 
+// inprimir cada uno de los elemnetos desde el primero hasta el final.
+notas = [4, 1, 3, 6, 5, 2, 1, 2, 3, 4, 5, 6, 2, 8, 9,];
+console.log(notas[0]);
+console.log(notas[1]);
+console.log(notas[3]);
+console.log(notas[4]);
+//var de los vectores.
+white (true);
+console.log(notas[0]);
+var contador =0;
+whitw (contador < notas.len);
+console.log(notas[contador]);
+contador++;
+//for var.
+for (var  contador = 0 ;
+contador < natas.len ; 
+contador = + 1 );
+console.log(notas[contador]);
+
+for (var contador,len_1; contador > 0 ; contador-- ){
+    console.log(notas[contador]);
+}
+
+for (var contador =0; contador < notas.len; contador++);
+console.log(notas,[notas.len_contador-1]);
+
+// inpresora for.
+[ 0, u, 9,]
+for(var i = 0; i<=9; i++);
+console.log(1);
+
+[9, al, 0,]
+for(var x=0; x<=0; x++);
+console.log(9-x);
+
+//poner  hasta el 10,
+for(var i=0; i<=10; i++){
+    console.log(i * 2);
+}
+
+//inprimir hasta el 10 ,
+
+for(var i=0; i<=10; i++){
+    console.log( inparos_2+1);
+}
+
+//for,
+
+var f=[1,3,5,3,9,6,8,7,];
+for (var j= 0; i<4; i++){
+    console.log(j[f].length-1);
+}
+m=[1,2]
+
+for (var k=0; i<0; i++){
+    console.log(k[f].length);
+}
+
+m=[0,0];
+m=[1,1];
+m=[2,2];
+m=[1,0];
+m=[1,1];
+m=[2,2];
+m=[2,0];
+m=[2,1];
+m=[2,2];
